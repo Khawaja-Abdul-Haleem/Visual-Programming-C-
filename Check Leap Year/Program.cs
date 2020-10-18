00357d08f9079552f70ce5baba57ef751dd20aa9
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-18 17:58:36 +0500

    Add files via upload

---
+++ Check Leap Year/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Check_Leap_Yeay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+
+            Console.Write("\t\t...Check LEAP YEAR....\n");
+            Console.Write("......................................\n");
+            Console.Write("Enter Year: ");
+            //year = Convert.ToInt32(Console.ReadLine());
+            year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.Write("\nIt's a Leap YEAR");
+                Console.ReadKey();
+            }
+
+            else if (year % 100 == 0)
+            {
+                Console.Write("\nIt's a Leap YEAR");
+                Console.ReadKey();
+            }
+
+            else if (year % 400 == 0)
+            {
+                Console.Write("\nIt's a Leap YEAR");
+                Console.ReadKey();
+            }
+
+            else {
+                Console.Write("\nIt's not a Leap YEAR");
+                Console.ReadKey();
+            }
+        }
+    }
+}
