91271c91953392fac587a56d9d08279e0f262fad
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-18 20:43:31 +0500

    Add files via upload

---
+++ Check Vowel or consonent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Check_Vowel_or_consonent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alpha;
+
+            Console.Write("Enter Alphabet: ");
+            alpha = Convert.ToChar(Console.ReadLine());
+
+            if (alpha == 'a' || alpha == 'A' || alpha == 'e' || alpha == 'E' || alpha == 'i' || alpha == 'I' || alpha == 'o' || alpha == 'O' || alpha == 'u' || alpha == 'U') {
+                Console.Write("\nThe Character is Vowel");
+            }
+
+            else
+            {
+                Console.Write("\n The Character is Consonent");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
