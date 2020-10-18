556ca55568a56af5cdb8e7beb891aa844dda35ef
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-19 00:43:27 +0500

    Add files via upload

---
+++ Display pattern of number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Display_n_number_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+           
+                Console.Write("Enter a number: ");
+
+                num = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= num; i++)
+                {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+
+                for (int k = (i - 1); k >= 1; k--)
+                    {
+
+                        Console.Write(k);
+                    }
+
+                    Console.WriteLine(); // for next line
+       
+            }
+            Console.ReadKey();
+        }
+    }
+}
