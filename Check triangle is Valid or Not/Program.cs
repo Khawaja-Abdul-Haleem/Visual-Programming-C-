0a9b68ab2175338e7c716deae04ad733090d9ade
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-18 20:53:43 +0500

    Add files via upload

---
+++ Check triangle is Valid or Not/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Check_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, sum = 0;
+
+            Console.Write("Enter X: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Y: ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Z: ");
+            z = int.Parse(Console.ReadLine());
+
+            sum = x + y + z;
+
+            if (sum == 180)
+            {
+
+                Console.Write("Triangle is Valid");
+            }
+            else
+            {
+                Console.Write("Triangle is not Valid");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
