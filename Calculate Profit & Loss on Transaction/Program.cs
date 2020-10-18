3eb2223dfdc4fb5909892e3e0997745a69c596ea
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-18 20:35:52 +0500

    Add files via upload

---
+++ Calculate Profit & Loss on Transaction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Profit_loss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cost_price, sell_price, result = 0;
+
+            Console.Write("Enter Cost Price: ");
+            cost_price = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Sell Price: ");
+            sell_price = int.Parse(Console.ReadLine());
+
+            if (cost_price > sell_price)
+            {
+                result = cost_price - sell_price;
+                Console.Write("\nYou Got A loss of Amount: " + result);
+
+            }
+
+            else if (sell_price > cost_price) {
+                result = sell_price - cost_price;
+                Console.Write("\n You can Booked your proit amount: " + result);
+            }
+
+            else
+            {
+                Console.Write("\n You are running in No Profit & No Loss Condition..");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
