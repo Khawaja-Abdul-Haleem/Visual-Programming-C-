d2eec7ed16b2aeadfc9f4a60bf62789ae4c19868
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-19 00:52:45 +0500

    Add files via upload

---
+++ Number into Reverse order/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace number_in_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, reverse = 0, rem;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+            Console.ReadKey();
+        }
+        
+    }
+}
