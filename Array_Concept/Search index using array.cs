09a5d202aa576bf096e8f69e3b7cba07c008d8d0
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-18 17:01:51 +0500

    Add files via upload

---
+++ Array_Concept/Search index using array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Search_Index_Using_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            int search_no;
+            bool flag = false;
+
+            Console.WriteLine("  Enter 10 numbers \n");
+
+            for (int i = 0; i < num.Length; i++) {
+                Console.Write("Number " + i + ": ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("\nEnter No. To Search: ");
+            search_no = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num.Length; i++) {
+                if (num[i] == search_no) {
+                    Console.Write("\nFound At Index: " + i + "\n" );
+                    flag = true;
+                }
+            } // for closing bracket
+
+            if (flag == false) {
+                Console.WriteLine("\nNot Found");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
