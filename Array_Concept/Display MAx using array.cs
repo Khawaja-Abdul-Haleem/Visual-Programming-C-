646437f4b9df8f96b0ccc1760e74962d8b59e1e4
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-18 17:27:38 +0500

    Add files via upload

---
+++ Array_Concept/Display MAx using array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Display_maximum_value_using_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int max = 0;
+
+            Console.WriteLine("   Enter 10 numbers\n");
+
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write("Enter Number " + i + ": ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+
+            Console.WriteLine("\n MAX = " + max);
+            Console.ReadKey();
+        }
+    }
+}
