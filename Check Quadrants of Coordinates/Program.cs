5d49b3eb3bb69ff342084508bf454e253abd704c
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-18 21:05:18 +0500

    Add files via upload

---
+++ Check Quadrants of Coordinates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Check_Quadrant_of_Coordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.Write("\n\n");
+            Console.Write("Find the quadrant in which the coordinate point lies:\n");
+            Console.Write("------------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input the value for X coordinate :");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Input the value for Y coordinate :");
+            y = int.Parse(Console.ReadLine());
+
+
+            if (x > 0 && y > 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the First quandrant.\n\n", x, y);
+            else if (x < 0 && y > 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Second quandrant.\n\n", x, y);
+            else if (x < 0 && y < 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Third quandrant.\n\n", x, y);
+            else if (x > 0 && y < 0)
+                Console.Write("The coordinate point ({0} {1}) lies in the Fourth quandrant.\n\n", x, y);
+            else if (x == 0 && y == 0)
+                Console.Write("The coordinate point ({0} {1}) lies at the origin.\n\n", x, y);
+
+            Console.ReadKey();
+        }
+    }
+}
