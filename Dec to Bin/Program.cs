0fbc8c4324a8dcb04dd98ff43913d4765940910a
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-19 21:05:40 +0500

    Add files via upload

---
+++ Dec to Bin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dec___bin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, j, binno = 0, dn;
+            Console.Write("\n\n");
+            Console.Write("Convert a decimal number to binary without using array:\n");
+            Console.Write("---------------------------------------------------------");
+            Console.Write("\n\n");
+            Console.Write("Enter a number to convert : ");
+            n = int.Parse(Console.ReadLine());
+            dn = n;
+            i = 1;
+            for (j = n; j > 0; j = j / 2)
+            {
+                binno = binno + (n % 2) * i;
+                i = i * 10;
+                n = n / 2;
+            }
+
+            Console.Write("\nThe Binary of {0} is {1}.\n", dn, binno);
+            Console.ReadKey();
+        }
+    }
+}
