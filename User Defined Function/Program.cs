ec5a6da045713aa414c90470766d246e50c96f99
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-20 18:46:08 +0500

    Add files via upload

---
+++ User Defined Function/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace User_Defined_Function
+{
+    class Program
+    {
+        public static void welcome(String name) {
+            Console.Write("Welcome " + name + "!\n");
+        }
+
+        public static void greeting() {
+            Console.Write("Have a Nice Day!");
+        }
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Enter a Name: ");
+            name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            welcome(name);
+            greeting();
+
+            Console.ReadKey();
+        }
+    }
+}
