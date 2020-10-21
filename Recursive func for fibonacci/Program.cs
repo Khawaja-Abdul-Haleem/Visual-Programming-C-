5540fa1d192b1b0c28657ab45938d8b8ca6ad034
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-10-21 10:49:17 +0500

    Add files via upload

---
+++ Recursive func for fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Recursive_function_for_fibonacci
+{
+   
+    class Program
+    {
+        public static int Fib(int n1)
+        {
+            //if ( (n1 == 1) || (number == 2) )
+            if (n1 <= 2)
+                return 1;
+            else
+                return Fib(n1 - 1) + Fib(n1 - 2);
+        }
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("\n\nRecursive Function : To calculate the Fibonacci number of a specific term :\n");
+            Console.Write("-------------------------------------------------------------------------------\n");
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nThe Fibonacci of {0} th term  is {1} \n", num, Fib(num));
+            Console.ReadKey();
+        }
+    }
+}
