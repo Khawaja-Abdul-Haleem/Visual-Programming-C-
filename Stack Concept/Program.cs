6647a844aa0db25b1863892e9f1ae6adaf6f316d
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-11-03 12:45:06 +0500

    Add files via upload

---
+++ Stack Concept/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> Book = new Stack<string>();
+            Book.Push("BOOK 1");
+            Book.Push("BOOk 2");
+
+            string top = Book.Peek();
+
+            int count = Book.Count();
+            Console.WriteLine("Count: " + count);
+
+            Console.WriteLine("TOP: " + top);
+            Console.WriteLine(Book.Pop());
+            Console.WriteLine(Book.Pop());
+
+            int count1 =  Book.Count();
+            Console.WriteLine("Count: " + count1);
+
+            top = Book.Peek();
+
+            Console.WriteLine("TOP: " + top);
+            // Console.WriteLine("")
+            Console.ReadKey();
+        }
+    }
+}
