acefd597e8ce0e682f0f2e5d8af6b29e97d20dc2
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-11-03 10:42:25 +0500

    Add files via upload

---
+++ Generic Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generic_Function
+{
+    class Program
+    {
+        static void Compare <T> (T a, T b) {
+            if (a.Equals(b))
+                Console.WriteLine("Equals");
+            else
+                Console.WriteLine("Not Equals");
+        }
+        static void Main(string[] args)
+        {
+            int x = 21, y = 21;
+            string s1 = "Hello", s2 = "World";
+
+            Compare<int>(x, y);
+            Compare<string>(s1, s2);
+
+            Console.ReadKey();
+        }
+    }
+}
