66401fa9ae5d08e6fb5e45dd644ca23643d05cf7
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-11-03 10:29:48 +0500

    Add files via upload

---
+++ Foreach Loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Foreach_Loop_Concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+
+            for (int i = 0; i < 5; i++) {
+                Console.Write("Element " + i + ": ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\t\t| OUTPUT | ");
+            foreach (int a in array) {
+                Console.WriteLine(a);
+            } // foreach loop end bracket
+
+            Console.ReadKey();
+        }
+    }
+}
