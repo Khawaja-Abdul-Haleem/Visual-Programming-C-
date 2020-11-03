684ea5e2ee678cbdd230733363a4f238e9f7c0d9
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-11-03 13:32:58 +0500

    Add files via upload

---
+++ List_concept/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> new_list= new List<string>();
+            new_list.Add("Ahmed");
+            new_list.Add("Qasim");
+            new_list.Add("Khalid");
+            new_list.Add("Usaama");
+            new_list.Add("Faisal");
+            new_list.Add("Uzair");
+
+            foreach (string items in new_list)
+            {
+                Console.WriteLine(items);
+            }
+
+            Console.WriteLine("\n_______________________________________________\n");
+            bool find = new_list.Contains("Khalid");
+            Console.WriteLine("Find Khalid: " + find);
+
+            double total = new_list.Count();
+            Console.WriteLine("Total: " + total);
+
+            string element = new_list.ElementAt(2);
+            Console.WriteLine("ELEMENT AT 2 INDEX: " + element);
+
+            Console.WriteLine("Remove Khalid: " + new_list.Remove("Khalid"));
+            new_list.RemoveAt(0);
+            Console.WriteLine("Removed item at index 0");
+
+            Console.WriteLine("Reverse List");
+            new_list.Reverse();
+
+            Console.WriteLine();
+            foreach (string items in new_list)
+            {
+                Console.WriteLine(items);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
