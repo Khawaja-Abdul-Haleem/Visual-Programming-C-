32c08d526a2628dbfb84f544c5337a1119dffe13
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-11-03 08:37:14 +0500

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Inheritance {
+    class SuperClass
+    { // Parent class 
+        public virtual void output()
+        {
+            Console.WriteLine("In Super Class..");
+        }
+    }
+}
+
+namespace Inheritance {
+    class SubClass : SuperClass // Child Class
+    {
+        public override void output()
+        {
+            base.output();
+            Console.WriteLine("In Sub Class...");
+        }
+    }
+}
+
+namespace Inheritance
+{
+  
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            SuperClass obj = new SubClass();
+            obj.output();
+
+           Console.ReadKey();
+        }
+    }
+}
