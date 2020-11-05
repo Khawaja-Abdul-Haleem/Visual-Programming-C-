638b33caa2479dd3145ea07389fced9a133e841f
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-11-05 15:20:55 +0500

    Add files via upload

---
+++ Temperature Calculator using Form/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Temperature_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnterTempLabel = new System.Windows.Forms.Label();
+            this.temConvLabel = new System.Windows.Forms.Label();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.rbCelcius = new System.Windows.Forms.RadioButton();
+            this.rbFaren = new System.Windows.Forms.RadioButton();
+            this.bnConv = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EnterTempLabel
+            // 
+            this.EnterTempLabel.AutoSize = true;
+            this.EnterTempLabel.Location = new System.Drawing.Point(34, 61);
+            this.EnterTempLabel.Name = "EnterTempLabel";
+            this.EnterTempLabel.Size = new System.Drawing.Size(113, 13);
+            this.EnterTempLabel.TabIndex = 0;
+            this.EnterTempLabel.Text = "Enter the Temperature";
+            this.EnterTempLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // temConvLabel
+            // 
+            this.temConvLabel.AutoSize = true;
+            this.temConvLabel.Location = new System.Drawing.Point(88, 27);
+            this.temConvLabel.Name = "temConvLabel";
+            this.temConvLabel.Size = new System.Drawing.Size(116, 13);
+            this.temConvLabel.TabIndex = 1;
+            this.temConvLabel.Text = "Temperature Converter";
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Location = new System.Drawing.Point(153, 61);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(100, 20);
+            this.txtTemperature.TabIndex = 2;
+            // 
+            // rbCelcius
+            // 
+            this.rbCelcius.AutoSize = true;
+            this.rbCelcius.Location = new System.Drawing.Point(177, 100);
+            this.rbCelcius.Name = "rbCelcius";
+            this.rbCelcius.Size = new System.Drawing.Size(117, 17);
+            this.rbCelcius.TabIndex = 3;
+            this.rbCelcius.Text = "Celcius (T-32) * 5/9";
+            this.rbCelcius.UseVisualStyleBackColor = true;
+            // 
+            // rbFaren
+            // 
+            this.rbFaren.AutoSize = true;
+            this.rbFaren.Checked = true;
+            this.rbFaren.Location = new System.Drawing.Point(12, 100);
+            this.rbFaren.Name = "rbFaren";
+            this.rbFaren.Size = new System.Drawing.Size(136, 17);
+            this.rbFaren.TabIndex = 4;
+            this.rbFaren.TabStop = true;
+            this.rbFaren.Text = "Fahrenheit (T*9/5) + 32";
+            this.rbFaren.UseVisualStyleBackColor = true;
+            // 
+            // bnConv
+            // 
+            this.bnConv.Location = new System.Drawing.Point(72, 142);
+            this.bnConv.Name = "bnConv";
+            this.bnConv.Size = new System.Drawing.Size(75, 23);
+            this.bnConv.TabIndex = 5;
+            this.bnConv.Text = "Convert";
+            this.bnConv.UseVisualStyleBackColor = true;
+            this.bnConv.Click += new System.EventHandler(this.bnConv_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Enabled = false;
+            this.txtResult.Location = new System.Drawing.Point(55, 189);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bnConv);
+            this.Controls.Add(this.rbFaren);
+            this.Controls.Add(this.rbCelcius);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.temConvLabel);
+            this.Controls.Add(this.EnterTempLabel);
+            this.Name = "Form1";
+            this.Text = "Temperature Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EnterTempLabel;
+        private System.Windows.Forms.Label temConvLabel;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.RadioButton rbCelcius;
+        private System.Windows.Forms.RadioButton rbFaren;
+        private System.Windows.Forms.Button bnConv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Temperature Calculator using Form/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperature_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bnConv_Click(object sender, EventArgs e)
+        {
+            int temp = int.Parse(txtTemperature.Text);
+            if (rbFaren.Checked)
+            {
+                if (txtTemperature.Text != string.Empty)
+                {
+                    double res = (temp * 1.8) + 32;
+                   // MessageBox.Show("Temperature: " + res );
+                    txtResult.Text = res.ToString();
+                }
+                else
+                    MessageBox.Show("Enter Temperature First");
+            } // if ending bracket
+
+            else if (rbCelcius.Checked) {
+                if (txtTemperature.Text != string.Empty)
+                {
+                    double res = (temp - 32) * 1.8;
+                    //MessageBox.Show("Temperature: " + res);
+                    txtResult.Text = res.ToString();
+                }
+                else
+                    MessageBox.Show("Enter Temperature First");
+            }
+        }
+    }
+}
