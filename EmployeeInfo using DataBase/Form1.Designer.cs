1cdfcf6d95e4621d27f6397839a68b9ae2746cb2
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-12-15 21:32:26 +0500

    Add files via upload

---
+++ EmployeeInfo using DataBase/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace StudentInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_conn = new System.Windows.Forms.Button();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_conn
+            // 
+            this.btn_conn.Location = new System.Drawing.Point(87, 174);
+            this.btn_conn.Name = "btn_conn";
+            this.btn_conn.Size = new System.Drawing.Size(75, 23);
+            this.btn_conn.TabIndex = 0;
+            this.btn_conn.Text = "Connect";
+            this.btn_conn.UseVisualStyleBackColor = true;
+            this.btn_conn.Click += new System.EventHandler(this.btn_conn_Click);
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(211, 174);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(75, 23);
+            this.btn_insert.TabIndex = 1;
+            this.btn_insert.Text = "Insert";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(335, 174);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Exit.TabIndex = 2;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(218, 59);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(182, 20);
+            this.tb_id.TabIndex = 5;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(218, 105);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(182, 20);
+            this.tb_name.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 450);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.btn_conn);
+            this.Name = "Form1";
+            this.Text = "StudentInfo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_conn;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.TextBox tb_name;
+    }
+}
+
---
+++ EmployeeInfo using DataBase/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StudentInfo
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Students;Data Source=HAIER-PC\\KAHSQL");
+
+        }
+
+        private void btn_conn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                 con.Open();
+                MessageBox.Show("SQL Connection Open");
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "insert into StudentInfo (StudentID,StudentName) values (" + tb_id.Text + ", '" + tb_name.Text + "')";
+                SqlCommand cmd = new SqlCommand(q, con);
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show(i + " Record Inserted");
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
