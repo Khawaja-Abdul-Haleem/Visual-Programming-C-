105078bcfe7e30a1639c12aedcff1d7e9bbdf924
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-12-22 21:37:01 +0500

    Add files via upload

---
+++ Fetch Info From Database/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace fetch_insert_delete_info_from_DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_price = new System.Windows.Forms.TextBox();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.btn_fetch = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(101, 62);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(165, 20);
+            this.tb_id.TabIndex = 3;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(101, 97);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(165, 20);
+            this.tb_name.TabIndex = 4;
+            // 
+            // tb_price
+            // 
+            this.tb_price.Location = new System.Drawing.Point(101, 132);
+            this.tb_price.Name = "tb_price";
+            this.tb_price.Size = new System.Drawing.Size(165, 20);
+            this.tb_price.TabIndex = 5;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(393, 60);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 6;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(53, 186);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(75, 23);
+            this.btn_insert.TabIndex = 8;
+            this.btn_insert.Text = "Insert";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(143, 186);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 9;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(236, 186);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(75, 23);
+            this.btn_del.TabIndex = 10;
+            this.btn_del.Text = "Delete";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // btn_fetch
+            // 
+            this.btn_fetch.Location = new System.Drawing.Point(303, 60);
+            this.btn_fetch.Name = "btn_fetch";
+            this.btn_fetch.Size = new System.Drawing.Size(75, 23);
+            this.btn_fetch.TabIndex = 11;
+            this.btn_fetch.Text = "Fetch";
+            this.btn_fetch.UseVisualStyleBackColor = true;
+            this.btn_fetch.Click += new System.EventHandler(this.btn_fetch_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(344, 105);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(77, 47);
+            this.btn_exit.TabIndex = 12;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_fetch);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.tb_price);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_price;
+        private System.Windows.Forms.Button btn_reset;
+       // private System.Windows.Forms.Button btb_fetch;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Button btn_fetch;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ Fetch Info From Database/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace fetch_insert_delete_info_from_DB
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Students;Data Source=HAIER-PC\\KAHSQL");
+        }
+
+        private void btn_fetch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string q = "select Name, Price from Products WHERE ID = "+tb_id.Text+"";
+                SqlCommand cmd = new SqlCommand(q, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    tb_name.Text = reader.GetValue(0).ToString();
+                    tb_price.Text = reader.GetValue(1).ToString();
+                    btn_insert.Enabled = false;
+                    tb_id.Enabled = false;
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            tb_id.Text = "";
+            tb_name.Text = "";
+            tb_price.Text = "";
+            btn_insert.Enabled = true;
+            tb_id.Enabled = true;
+        }
+
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string q = "INSERT INTO Products (ID, Name, Price) values (" + tb_id.Text + ", '" + tb_name.Text + "', " +tb_price.Text+ ")";
+                SqlCommand cmd = new SqlCommand(q, con);
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show(i + " Record Inserted");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string q = "UPDATE Products set Name = '" + tb_name.Text + "' , Price = " + tb_price.Text + " where ID = " + tb_id.Text + "";
+                SqlCommand cmd = new SqlCommand(q, con);
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show(i + " Record Updated");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string q = "delete from Products where ID = " + tb_id.Text + "";
+                SqlCommand cmd = new SqlCommand(q, con);
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show(i + " Record Deleted");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
