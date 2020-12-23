13eb96962e871d15eb7819c3db11ba743ba65335
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-12-23 22:13:15 +0500

    Add files via upload

---
+++ Update Db from Locally DB/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace update_data_in_Database_from_Locally
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.btn_get_data = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(162, 75);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(237, 20);
+            this.tb_id.TabIndex = 0;
+            // 
+            // btn_get_data
+            // 
+            this.btn_get_data.Location = new System.Drawing.Point(428, 75);
+            this.btn_get_data.Name = "btn_get_data";
+            this.btn_get_data.Size = new System.Drawing.Size(75, 23);
+            this.btn_get_data.TabIndex = 3;
+            this.btn_get_data.Text = "Get Daa";
+            this.btn_get_data.UseVisualStyleBackColor = true;
+            this.btn_get_data.Click += new System.EventHandler(this.btn_get_data_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(428, 111);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 40);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(162, 114);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(237, 20);
+            this.tb_name.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Student ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Student Name";
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(303, 165);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(77, 35);
+            this.btn_update.TabIndex = 8;
+            this.btn_update.Text = "Update on DB";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(176, 165);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(77, 35);
+            this.btn_insert.TabIndex = 9;
+            this.btn_insert.Text = "Insert";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(159, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_get_data);
+            this.Controls.Add(this.tb_id);
+            this.Name = "Form1";
+            this.Text = "Local Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Button btn_get_data;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Update Db from Locally DB/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace update_data_in_Database_from_Locally
+{
+    
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        DataSet ds;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Students;Data Source=HAIER-PC\\KAHSQL");
+            ds = new DataSet();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_get_data_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                string q = "select * from StudentInfo";
+                SqlCommand cmd = new SqlCommand(q, con);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                adp.Fill(ds);
+                con.Close();
+
+                dataGridView1.DataSource = ds.Tables[0];
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds.Tables[0].Rows.Add(tb_id.Text, tb_name.Text);
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                string q = "select * from StudentInfo";
+                SqlCommand cmd = new SqlCommand(q, con);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+
+                SqlCommandBuilder cmd_build = new SqlCommandBuilder(adp);
+                adp.Update(ds.Tables[0]);
+                ds.Tables[0].AcceptChanges();
+                MessageBox.Show("Updated On DataBase SuccessFully");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
