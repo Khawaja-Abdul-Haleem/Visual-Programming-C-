c7e66c9896010f7eabf4b2b0a201f96cf6ff6b36
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2020-12-23 20:32:39 +0500

    Add files via upload

---
+++ Database Connectivity (DISCONNECTED)/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Database_Diconnected_Approach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.col_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_get_data = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_search
+            // 
+            this.tb_search.Location = new System.Drawing.Point(128, 47);
+            this.tb_search.Name = "tb_search";
+            this.tb_search.Size = new System.Drawing.Size(248, 20);
+            this.tb_search.TabIndex = 0;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(397, 45);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 1;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_id,
+            this.col_name});
+            this.dataGridView1.Location = new System.Drawing.Point(128, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(248, 170);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // col_id
+            // 
+            this.col_id.HeaderText = "ID";
+            this.col_id.Name = "col_id";
+            // 
+            // col_name
+            // 
+            this.col_name.HeaderText = "Student Name";
+            this.col_name.Name = "col_name";
+            // 
+            // btn_get_data
+            // 
+            this.btn_get_data.Location = new System.Drawing.Point(128, 18);
+            this.btn_get_data.Name = "btn_get_data";
+            this.btn_get_data.Size = new System.Drawing.Size(75, 23);
+            this.btn_get_data.TabIndex = 3;
+            this.btn_get_data.Text = "Get Data";
+            this.btn_get_data.UseVisualStyleBackColor = true;
+            this.btn_get_data.Click += new System.EventHandler(this.btn_get_data_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(397, 86);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_get_data);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.tb_search);
+            this.Name = "Form1";
+            this.Text = "Database offline";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_name;
+        private System.Windows.Forms.Button btn_get_data;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ Database Connectivity (DISCONNECTED)/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Database_Diconnected_Approach
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        DataSet ds;
+       // DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Students;Data Source=HAIER-PC\\KAHSQL");
+            ds = new DataSet(); // we can use dataset and datatable
+            //for single table use
+            // dt = new Datatable();
+
+      // Note:  ds is used for multiple tables
+
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow[] dr = ds.Tables[0].Select("StudentName like '%" + tb_search.Text + "%' ");
+                dataGridView1.Rows.Clear();
+                foreach (DataRow r in dr)
+                {
+                    dataGridView1.Rows.Add(r[0].ToString(), r[1].ToString());
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_get_data_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                string q = "select * from StudentInfo";
+                SqlCommand cmd = new SqlCommand(q, con);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                adp.Fill(ds);
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
