7ef5dc7517de5bf28f4d6c98bece014ffd214347
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2021-01-12 16:22:14 +0500

    Add files via upload

---
+++ LogFiles/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Filing__via_form_LogFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.tb_phone = new System.Windows.Forms.TextBox();
+            this.tb_nic = new System.Windows.Forms.TextBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(76, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NIC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(76, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone #";
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(139, 203);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(140, 42);
+            this.btn_save.TabIndex = 4;
+            this.btn_save.Text = "Save Records";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(315, 203);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(140, 42);
+            this.btn_exit.TabIndex = 5;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // tb_phone
+            // 
+            this.tb_phone.Location = new System.Drawing.Point(229, 156);
+            this.tb_phone.Name = "tb_phone";
+            this.tb_phone.Size = new System.Drawing.Size(281, 20);
+            this.tb_phone.TabIndex = 6;
+            // 
+            // tb_nic
+            // 
+            this.tb_nic.Location = new System.Drawing.Point(229, 126);
+            this.tb_nic.Name = "tb_nic";
+            this.tb_nic.Size = new System.Drawing.Size(281, 20);
+            this.tb_nic.TabIndex = 7;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(229, 96);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(281, 20);
+            this.tb_name.TabIndex = 8;
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(229, 66);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(281, 20);
+            this.tb_id.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.tb_nic);
+            this.Controls.Add(this.tb_phone);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LogFile Concept";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox tb_phone;
+        private System.Windows.Forms.TextBox tb_nic;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_id;
+    }
+}
+
---
+++ LogFiles/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Filing__via_form_LogFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tb_name.Text.Contains("0") || tb_name.Text.Contains("1") ||
+                    tb_name.Text.Contains("2") || tb_name.Text.Contains("3") ||
+                    tb_name.Text.Contains("4") || tb_name.Text.Contains("5") ||
+                    tb_name.Text.Contains("6") || tb_name.Text.Contains("7") ||
+                    tb_name.Text.Contains("8") || tb_name.Text.Contains("9"))
+                {
+                    throw new Exception("Invalid Name");
+                }
+
+                int id = int.Parse(tb_id.Text);
+                double nic = double.Parse(tb_nic.Text);
+                double phone = double.Parse(tb_phone.Text);
+
+            }
+            catch (Exception ex) {
+                FileStream file = new FileStream("Log.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter stream_writer = new StreamWriter(file);
+                stream_writer.Write(DateTime.Now + "|" + ex.Message +"|" + "LogFileConcept ---> Save"+"~");
+                stream_writer.Close();
+                file.Close();
+
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
